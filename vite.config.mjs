37d3613d649b6daaac109dff23fed7ada0c7ebd4
TOT <165574113+tolutally@users.noreply.github.com>
2026-05-12 19:42:12 -0300

    Fix Vercel build output directory

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// Simplified config for development
+// Simplified config for development and Vercel deployments
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
   plugins: [tsconfigPaths(), react()],
   server: {
